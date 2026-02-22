7813d03d3d56871855d7642206e2213f524302b8
tm2533 <tm2533@bath.ac.uk>
2026-02-22 12:49:02 +0000

    Solve questions on Nested Queries

--- queries.sql
+++ queries.sql
@@ -1,6 +1,9 @@
+------------------------------------------------------------------------------------------
 -- Some queries to try
+------------------------------------------------------------------------------------------
 -- 1. What are the names of employees in the Marketing Department?
-SELECT EmployeeName FROM Employee
+SELECT EmployeeName 
+FROM Employee
 WHERE DepartmentName = 'Marketing'
 ORDER BY EmployeeName;
 
@@ -13,6 +16,26 @@ WHERE DepartmentName IN (
     WHERE DepartmentFloor = 2
 )
 ORDER BY DepartmentName, SaleQuantity DESC, ItemName;
+
+-- Avoid the nested subquery
+SELECT DISTINCT S.ItemName
+FROM Sale AS S
+LEFT JOIN Department AS D
+    ON S.DepartmentName = D.DepartmentName
+WHERE D.DepartmentFloor = 2
+ORDER BY S.ItemName;
+
+-- Use natural join 
+SELECT DISTINCT ItemName
+FROM (Sale NATURAL JOIN Department)
+WHERE Department.DepartmentFloor = 2
+ORDER BY ItemName;
+
+-- User join - Notice different results
+SELECT DISTINCT ItemName
+FROM (Sale JOIN Department)
+WHERE Department.DepartmentFloor = 2
+ORDER BY ItemName;
 
 -- 3. Identify by floor the items available on floors other than the second floor
 SELECT S.*, D.DepartmentFloor
@@ -97,3 +120,100 @@ LEFT JOIN Supplier AS S
 WHERE I.ItemType = 'N'
 GROUP BY S.SupplierName
 ORDER BY avg_delivery_quantity DESC;
+
+------------------------------------------------------------------------------------------
+-- Nested Queries
+------------------------------------------------------------------------------------------
+-- 1. What are the names of items sold by departments on the second floor? This was previously
+-- solved in the preceding section by the use of a join. However, it could be more efficiently
+-- solved by using an inner query
+
+SELECT DISTINCT ItemName
+FROM Sale
+WHERE DepartmentName IN (
+    SELECT DISTINCT DepartmentName
+    FROM Department
+    WHERE DepartmentFloor = 2
+)
+ORDER BY 1;
+
+-- 2. Find the salary of Clare's manager.
+SELECT * FROM Employee LIMIT 5;
+
+SELECT EmployeeSalary, EmployeeName
+FROM Employee
+WHERE EmployeeNumber = (
+    SELECT BossNumber
+    FROM Employee
+    WHERE EmployeeName = 'Clare'
+);
+
+-- 4. List the names of the employees who earn more than any employee in the Marketing
+-- department
+SELECT EmployeeSalary, EmployeeName
+FROM Employee
+WHERE EmployeeSalary > (
+    SELECT MAX(EmployeeSalary)
+    FROM Employee
+    WHERE DepartmentName = 'Marketing'
+)
+ORDER BY EmployeeSalary DESC, EmployeeName;
+
+-- 5. Among all the departments with a total salary greater than £25000, 
+-- find the departments that sell Sextant
+SELECT DISTINCT DepartmentName
+FROM Sale
+WHERE ItemName = 'Sextant'
+AND DepartmentName IN (
+    SELECT DepartmentName
+    FROM Employee
+    GROUP BY DepartmentName
+    HAVING SUM(EmployeeSalary) > 25000
+)
+ORDER BY 1;
+
+-- 6. Find the suppliers that deliver compasses and at least one other kind of item
+SELECT DISTINCT DepartmentName
+FROM Sale
+WHERE DepartmentName IN (
+    SELECT DepartmentName
+    FROM Sale
+    GROUP BY DepartmentName
+    HAVING COUNT(DISTINCT ItemName) >= 3
+) AND DepartmentName IN ( 
+    SELECT DISTINCT DepartmentName
+    FROM Sale
+    WHERE ItemName = 'Compass'
+)
+ORDER BY DepartmentName
+;
+
+-- 7. Find the suppliers that deliver compasses and at least three other kinds of item
+SELECT DISTINCT D.SupplierNumber, S.SupplierName
+FROM Delivery AS D
+LEFT JOIN Supplier AS S
+    ON D.SupplierNumber = S.SupplierNumber
+WHERE D.SupplierNumber IN (
+    SELECT DISTINCT SupplierNumber
+    FROM Delivery
+    WHERE ItemName = 'Compass'
+) 
+AND D.SupplierNumber IN (
+    SELECT SupplierNumber
+    FROM Delivery
+    GROUP BY SupplierNumber
+    HAVING COUNT(DISTINCT ItemName) >= 4
+)
+ORDER BY S.SupplierName;
+
+-- 8. List the departments for which each item delivered to the department is delivered to some
+-- other department as well
+SELECT DISTINCT DepartmentName
+FROM Delivery
+WHERE ItemName IN (
+    SELECT ItemName
+    FROM Delivery
+    GROUP BY ItemName
+    HAVING COUNT(DISTINCT DepartmentName) >= 2
+)
+ORDER BY DepartmentName;
